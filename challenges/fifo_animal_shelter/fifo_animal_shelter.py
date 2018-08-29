a2ca6ca1d83ff8379d100bf182c07f53acc26622
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-08-29 00:34:18 -0700

    updated with code and tests

--- challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -1,6 +1,15 @@
-from ... data_structures.queue.queue import Node, Queue
+from .queue.queue import Node, Queue
 
-class Animal_Shelter(object):
+class Animal(object):
+    """ This will be an animal object to be used by Animal_Shelter
+    """
+    def __init__(self, pet_name, pet_type):
+        self.pet_name = pet_name
+        self.pet_type = pet_type
+        self.tag = 0
+
+
+class AnimalShelter(object):
     """ AnimalShelter which holds only dogs and cats.
         Users can select to adopt a dog, a cat, or no preference.
         The shelter operates using a first-in, first-out approach.
@@ -11,60 +20,60 @@ class Animal_Shelter(object):
         """
         self.front = None
         self.back = None
-        self._length = 0
         self.dogs = Queue()
         self.cats = Queue()
-
-        # if data is not None:
-        #     try:
-        #         vals = iter(data)
-        #     except TypeError:
-        #         vals = [data]
-        #     for i in vals:
-        #         newNode = Node(i)
-        #         if self.front is None:  # this is the first node in queue
-        #             self.front = newNode
-        #         elif self.back is None:  # this is the second node in queue
-        #             self.front._next, self.back = newNode, newNode
-        #         else:
-        #             self.back._next, self.back = newNode, newNode
-        #         self._length += 1
+        self._length = 0
+        self.tag = 0
+        # next animal will get this tag added to their object,
+        # essentially an incrementing count of incoming pets
+        # this also works as a total number of cats & dogs the
+        # shelter has had, but not for current total.
 
     def __len__(self):
         return self._length
 
     def __str__(self):
-        return f'Front: {self.front} | Back: {self.back} | Length: {self._length}'
+        return f'Total Tagged Animals: {self.tag} | Sheltered Animals: {self._length}'
 
     def __repr__(self):
-        return f'<Front: {self.front} | Back: {self.back} | Length: {self._length}>'
+        return f'<Total Intake: {self.tag} | Length: {self._length}>'
 
     def enqueue(self, animal):
         """ Adds animal to the shelter. animal can be either a dog or a cat object.
             On adding animal, increments _length. First in, First out.
         """
-        animal.type
-        newNode = Node(animal)
-        if self.front is None:  # this is the first node in queue
-            self.front = newNode
-        elif self.back is None:  # this is the second node in queue
-            self.front._next, self.back = newNode, newNode
+        if animal.pet_type == 'dog':
+            self.tag += 1
+            animal.tag = self.tag
+            self.dogs.enqueue(animal)
+            self._length += 1
+        elif animal.pet_type == 'cat':
+            self.tag += 1
+            animal.tag = self.tag
+            self.cats.enqueue(animal)
+            self._length += 1
         else:
-            self.back._next, self.back = newNode, newNode
-        self._length += 1
+            return f'Sorry, we only take dogs and cats here!'
 
-    def dequeue(self):
-        """ Returns the node that is in the front of the queue.
-            Removes it from the queue and deincrements the _length
+    def dequeue(self, pref):
+        """ The user may have a preference equal to 'dog' or 'cat', or 'any'.
+            if the user gave 'any' for preference, they will get whichever
+            animal the shelter has had the longest. Likewise, if they give
+            the other options for preference, they will get whichever of that
+            type of animal the shelter has had the longest.
         """
-        temp = self.front
-        self.front = self.front._next
-        temp._next = None
-        self._length -= 1
-        return temp
-
-    def peek(self):
-        return self.front
-
+        if pref == 'any':
+            if self.dogs.front.val.tag < self.cats.front.val.tag:
+                pref = 'dog'
+            elif self.cats.front.val.tag < self.dogs.front.val.tag:
+                pref = 'cat'
+        if pref == 'dog':
+            self._length -= 1
+            return self.dogs.dequeue()
+        elif pref == 'cat':
+            self._length -= 1
+            return self.cats.dequeue()
+        else:
+            return f'Sorry, we only give out dogs and cats here!'
 
 
