264167f712e8965ba50dc9a3777f5030748b232b
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-09-24 14:40:40 -0700

    starter files, whiteboard, basic setup

---
+++ challenges/repeated_word/repeated_word.py
@@ -0,0 +1,12 @@
+# from textwrap import dedent
+# import sys
+
+def repeated_word(string):
+    """ Return the first repeated word
+    """
+
+    return None
+
+
+if __name__ == '__main__':
+    string = input('Input words :>')
