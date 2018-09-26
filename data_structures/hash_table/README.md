104a776c534d5f0ce737784e3ffe12e30c5b00e4
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-09-25 18:10:35 -0700

    setup and start

---
+++ data_structures/hash_table/README.md
@@ -0,0 +1,15 @@
+# hash_table
+
+Implementing a new abstract data structure: Hash Table.
+
+## Challenge
+
+In hash_table.py:
+Complete the provided scaffold implementation for your Hash Table
+
+## Solution
+
+![Whiteboard Image](../../assets/hash_table.jpg)
+<!-- replace 'title_name' above with the image labeled accordingly -->
+
+**Author**: Chris L Chapman
---
+++ data_structures/hash_table/hash_table.py
@@ -0,0 +1,59 @@
+# from textwrap import dedent
+# import sys
+
+
+class HashTable:
+    """A class for a hash table."""
+    entries_count = 0
+    alphabet_size = 52
+
+    def __init__(self, size=8192):
+        self.table_size = size
+        self.hashtable = []
+
+    def __repr__(self):
+        pass
+
+    def _hash_key(self, key):
+        """Create a hash from a given key.
+        args:
+            key: a string to hash
+        returns: an integer corresponding to hashtable index
+        """
+        hash_ = 0
+        for i, c in enumerate(key):
+            hash_ += pow(
+                self.alphabet_size, len(key) - i - 1) * ord(c)
+        return hash_ % self.table_size
+
+    def set(self, key, value):
+        """Add a key and value into the hash table.
+        args:
+            key: the key to store
+            value: the value to store
+        """
+        hashkey = self._hash_key(key)
+        if self.hashtable[hashkey] is None:
+            self.hashtable[hashkey] = [value]
+        else:
+            # TODO: deal with collisions
+            pass
+        return True
+
+    def get(self, key):
+        """Retrieve a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        hashkey = self._hash_key(key)
+        # TODO: deal with multiple values for this hashkey
+        return self.hashtable[hashkey]
+
+    def remove(self, key):
+        """Retrieve and remove a value from the hash table by key.
+        args:
+            key: a string to find the value in the hash table
+        returns: the value stored with the key
+        """
+        pass
