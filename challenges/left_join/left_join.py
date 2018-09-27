9bde18c0438d433d5a638bfe515114d42f80f37b
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-09-26 18:45:51 -0700

    setup files

---
+++ challenges/left_join/left_join.py
@@ -0,0 +1,12 @@
+# from textwrap import dedent
+# import sys
+
+
+def left_join_hash(hash1, hash2):
+    """ Takes in two HashTables, returns a left join.
+    """
+    pass
+
+
+# if __name__ == '__main__':
+#     do stuff
