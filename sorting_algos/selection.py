9dc8cd881f3b913ef60f5ea4eca617d0faf059f4
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-09-28 14:36:49 -0700

    READMEs, starter file, scaffold tests

---
+++ sorting_algos/selection.py
@@ -0,0 +1,14 @@
+# from textwrap import dedent
+# import sys
+
+def selection_sort():
+    """ Sorts an array by repeatedly finding the minimum element (considering
+        ascending order) from unsorted part and putting it at the beginning.
+        The algorithm maintains two subarrays in a given array.
+    """
+    # do stuff
+    return
+
+
+# if __name__ == '__main__':
+#     do stuff
