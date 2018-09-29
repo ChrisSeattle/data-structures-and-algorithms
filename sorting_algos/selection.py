5a42a4f1e54f8e6d6feabb2921c3aeb04965a1e2
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-09-28 18:03:14 -0700

    work the code

--- sorting_algos/selection.py
+++ sorting_algos/selection.py
@@ -1,13 +1,22 @@
 # from textwrap import dedent
-# import sys
+import sys
 
-def selection_sort():
+
+def selection_sort(arr):
     """ Sorts an array by repeatedly finding the minimum element (considering
         ascending order) from unsorted part and putting it at the beginning.
         The algorithm maintains two subarrays in a given array.
+        This implementation is an in-place sort.
     """
-    # do stuff
-    return
+
+    for j in range(len(arr)-1):
+        min_idx = j
+        # import pdb; pdb.set_trace()
+        for i in range(j+1, len(arr)):
+            if arr[min_idx] > arr[i]:
+                min_idx = i
+            arr[j], arr[min_idx] = arr[min_idx], arr[j]
+    return arr
 
 
 # if __name__ == '__main__':
