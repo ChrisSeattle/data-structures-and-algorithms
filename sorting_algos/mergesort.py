dca684b10a6448d884c255d4be0a167338c1f6bc
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-10-14 16:01:11 -0700

    update with local code

--- sorting_algos/mergesort.py
+++ sorting_algos/mergesort.py
@@ -1,14 +1,42 @@
 
 def merge_sort(arr):
-    """
+    """ Implementation of a merge_sort algorithm using recursion.
     """
 
-    def _merge(A, B):
+    def _merge(a, b):
         """ This will be recalled recursevly.
+            Takes in two pre-sorted arrays and returns a single sorted array.
         """
+        i = 0
+        j = 0
+        k = 0
+        r = [[] for _ in range(len(a)+len(b))]
 
+        while i < len(a) and j < len(b):
+            # import pdb; pdb.set_trace()
+            if b[j] < a[i]:
+                r[k] = b[j]
+                j += 1
+            else:
+                r[k] = a[i]
+                i += 1
+            k += 1
+
+        while i < len(a):
+            r[k] = a[i]
+            k += 1
+            i += 1
+        while j < len(b):
+            r[k] = b[j]
+            k += 1
+            j += 1
+
+        return r
+        # end _merge helper function
+
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        a = merge_sort(arr[:mid])
+        b = merge_sort(arr[mid:])
+        return _merge(a, b)
     return arr
-
-
-# if __name__ == '__main__':
-#     do stuff
